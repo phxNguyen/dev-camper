e666c2b28cb63f5d86875a76fdd3b5087162361d
nhuphuoc <nhuphuoc3008@hotmail.com>
2022-02-10 16:10:05 +0700

    setting up route

---
+++ routes/bootcamps.js
@@ -0,0 +1,37 @@
+
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/',(req,res)=>{
+  res
+    .status(200)
+    .json({success:true,msg:'Get all bootcamps'})
+})
+
+router.get('/:id',(req,res)=>{
+  res
+    .status(200)
+    .json({success:true,msg:`Show bootcamps ${req.params.id}`})
+})
+
+router.post('/',(req,res)=>{
+  res
+    .status(200)
+    .json({success:true,msg:'Create a bootcamps'})
+})
+
+router.put('/:id',(req,res)=>{
+  res
+    .status(200)
+    .json({success:true,msg:`Update bootcamps ${req.params.id}`})
+})
+
+router.delete('/:id',(req,res)=>{
+  res
+    .status(200)
+    .json({success:true,msg:`Delete bootcamps ${req.params.id}`})
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//Route files
+const bootcamps = require('./routes/bootcamps')
+
 //loading env config
 dotenv.config({ path: './config/config.env' });
 const app = express();
 
-const PORT = process.env.PORT;
+// su dung router
+app.use('/api/v1/bootcamps',bootcamps)
 
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(
     `listening on port ${process.env.PORT} in ${process.env.NODE_ENV}`
