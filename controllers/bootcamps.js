97c6597736bce2d8d7b1572f73afac8c3ed3bca1
nhuphuoc <nhuphuoc3008@hotmail.com>
2022-03-01 10:37:32 +0700

    async middleware

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,55 +1,48 @@
 const Bootcamps = require('../database/models/Bootcamps');
 const ErrorResponse = require('../utils/errorResponse')
+const asyncHandler = require('../middleware/async')
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access    Public
-exports.getBootcamps = async(req,res,next)=>{
-  try{
-    const bootcamps = await Bootcamps.find();
-    res.status(200).json({success:true, count:bootcamps.length, data:bootcamps})
-  }catch(err){
-    next(err);
-  }
-}
+exports.getBootcamps = asyncHandler(async(req,res,next)=>{
+  
+  const bootcamps = await Bootcamps.find();
+  res
+    .status(200)
+    .json({success:true, count:bootcamps.length, data:bootcamps})
+
+})
 
 // @desc    Get single bootcamps
 // @route   GET /api/v1/bootcamps/:id
 // @access  Private
 
-exports.getBootcamp = async(req,res,next)=>{
+exports.getBootcamp = asyncHandler(async(req,res,next)=>{
   
-  try{
     const bootcamp = await Bootcamps.findById(req.params.id)
     if(!bootcamp){
       next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     };
     res.status(200).json({success:true,data:bootcamp})
-  }catch(err){
-    //res.status(400).json({success:false})
-    next(err);
-  }
-  
   
 }
+)
 
 // @desc    Create new bootcamps
 // @route   POST /api/v1/bootcamps/
 // @access  Private
 
-exports.createBootcamp = async(req,res,next)=>{
- try{ const bootcamp = await Bootcamps.create(req.body);
-  res.status(201).json({success:true, data:bootcamp})}
-  catch(err){
-    next(err);
-  }
-}
+exports.createBootcamp = asyncHandler(async(req,res,next)=>{
+   const bootcamp = await Bootcamps.create(req.body);
+   res.status(201).json({success:true, data:bootcamp})
+ }
+)
 
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
 
-exports.updateBootcamp = async(req,res,next)=>{
-  try{
+exports.updateBootcamp = asyncHandler(async(req,res,next)=>{
     const bootcamp = await Bootcamps.findByIdAndUpdate(req.params.id, req.body,{
       new: true,
       runValidators: true
@@ -60,37 +53,23 @@ exports.updateBootcamp = async(req,res,next)=>{
     }
 
     res.status(200).json({success: true, data: bootcamp})
-  }catch(err){
-<<<<<<< HEAD
-    next(err);
-=======
-    res.status(400).json({success:false})
->>>>>>> dbc5554f83284a9ebe7236917c703869c963a602
-  }
+  
 }
+)
 
 // @desc    Delete bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
 
-exports.deleteBootcamp = async(req,res,next)=>{
-  try{
-    const bootcamp = await Bootcamps.findByIdAndDelete(req.params.id)
-
-    if(!bootcamp){
-<<<<<<< HEAD
-      next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
-=======
-      return res.status(400).json({success: false})
->>>>>>> dbc5554f83284a9ebe7236917c703869c963a602
-    }
-
-    res.status(200).json({success: true, data: {}})
-  }catch(err){
-<<<<<<< HEAD
-    next(err);
-=======
-    res.status(400).json({success:false})
->>>>>>> dbc5554f83284a9ebe7236917c703869c963a602
+exports.deleteBootcamp = asyncHandler(
+  async(req,res,next)=>{
+      const bootcamp = await Bootcamps.findByIdAndDelete(req.params.id)
+  
+      if(!bootcamp){
+        next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+      }
+  
+      res.status(200).json({success: true, data: {}})
+    
   }
-}
+)
--- middleware/error.js
+++ middleware/error.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const ErrorResponse = require("../utils/errorResponse");
 
 const errorHandler = (err, req, res, next) =>{
@@ -6,7 +5,7 @@ const errorHandler = (err, req, res, next) =>{
   let error = { ...err};
   error.message = err.message;
   
-  console.log(err.stack);
+  console.log(err);
 
 
   // Khong tim thay Mongoose ObjectID
@@ -14,18 +13,22 @@ const errorHandler = (err, req, res, next) =>{
     const message = `Resource not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404)
   }
-   
+
+  ///Khi key bi trung` (trung` name)
+  if(err.code === 11000 ){
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 404)
+  }
+
+   // response server bi loi
   res.status(error.statusCode || 500).json({
     success: false,
     error: error.message || 'Server Error'
-=======
-const errorHandler = (err, req, res, next) =>{
-  console.log(err.stack);
-  res.status(500).json({
-    success: false,
-    error: err.message
->>>>>>> dbc5554f83284a9ebe7236917c703869c963a602
   });
+
+
+  
+  
 }
 
 module.exports = errorHandler;
